960bab8cdd7c8562107f76c918eac3666309c5d7
AurelienGttn <aurelien.guitton@hotmail.com>
2019-02-19 15:45:52 -0500

    Trigger sound on collision

---
+++ DrumVR/Assets/Scripts/Drumstick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drumstick : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hit trigger");
+        if (other.CompareTag("Drumset")){
+            other.GetComponent<AudioSource>().Play();
+        }
+    }
+}
