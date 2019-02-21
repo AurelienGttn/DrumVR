865b17385b07968b224927387d5a514574db6850
AurelienGttn <aurelien.guitton@hotmail.com>
2019-02-21 17:00:51 -0500

    Improve collision to trigger only from downward movement

--- DrumVR/Assets/Scripts/Drumstick.cs
+++ DrumVR/Assets/Scripts/Drumstick.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class Drumstick : MonoBehaviour
 {
     bool triggered = false;
-
+    bool wrongDirection = false;
     private void OnTriggerEnter(Collider other)
     {
+        // Make sure the stick exited the collision box
         if (!triggered)
         {
-            if (other.CompareTag("Drumset"))
+            // Check if the stick hit the top of the drum piece first
+            if (other.name == "DirectionCheck")
             {
+                Debug.Log("Hit bottom of: " + other.transform.parent.name);
+                wrongDirection = true;
+            }
+
+            // Play the sound of the drum piece if it hit the top first
+            else if (other.CompareTag("Drumset") && !wrongDirection)
+            {
+                Debug.Log("Play sound: " + other.transform.parent.name);
                 other.GetComponent<AudioSource>().Play();
                 triggered = true;
             }
@@ -20,6 +30,16 @@ public class Drumstick : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        triggered = false;
+        // Reset the direction check on exit
+        if (other.name == "DirectionCheck")
+        {
+            wrongDirection = false;
+        }
+
+        // Allow the stick to trigger another drum piece
+        if (other.CompareTag("Drumset"))
+        {
+            triggered = false;
+        }
     }
 }
