58350e1fcefdddd8217381038143fb3c221a66de
AurelienGttn <aurelien.guitton@hotmail.com>
2019-02-26 15:39:27 -0500

    Closes #2
    Add colliders to all drum pieces
    Add trigger to check drumstick direction
    Create script to handle collision and play sound

---
+++ DrumVR/Assets/Scripts/Drumset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drumset : MonoBehaviour
+{
+
+    private AudioSource source;
+    private AudioClip sound;
+    [SerializeField] private DirectionCheck directionCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        sound = GetComponent<AudioSource>().clip;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (directionCheck == null || !directionCheck.GetComponent<DirectionCheck>().wrongDirection)
+        {
+            source.PlayOneShot(sound);
+            Debug.Log(this.transform.parent.name);
+        }
+    }
+}
--- DrumVR/Assets/Scripts/Drumstick.cs
+++ DrumVR/Assets/Scripts/Drumstick.cs
@@ -1,28 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class Drumstick : MonoBehaviour
 {
-    bool triggered = false;
-    bool wrongDirection = false;
+    [SerializeField] private GameObject rightHand, leftHand;
+    private Vector3 velocityRH, velocityLH;
+
+    [SerializeField] private AudioClip clip;
+
+    private bool right = false, left = false;
+    private bool triggered = false;
+    private bool wrongDirection = false;
+
+    private void Start()
+    {
+        if (name == "Right Drumstick")
+            right = true;
+        else
+            left = true;
+    }
+
+    private void Update()
+    {
+        velocityLH = leftHand.GetComponent<VelocityEstimator>().GetAngularVelocityEstimate();
+        velocityRH = rightHand.GetComponent<VelocityEstimator>().GetAngularVelocityEstimate();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        //if (other.CompareTag("Drumset")) {
+        //    if ((right && velocityRH.y <= 0) || 
+        //        (left && velocityLH.y <= 0))
+        //    {
+        //        other.GetComponent<AudioSource>().Play();
+        //    }
+        //}
+
         // Make sure the stick exited the collision box
-        if (!triggered)
+        if (!triggered && !wrongDirection)
         {
-            // Check if the stick hit the top of the drum piece first
+            // Check if the stick hit the bottom of the drum piece first
             if (other.name == "DirectionCheck")
             {
-                Debug.Log("Hit bottom of: " + other.transform.parent.name);
                 wrongDirection = true;
             }
 
             // Play the sound of the drum piece if it hit the top first
-            else if (other.CompareTag("Drumset") && !wrongDirection)
+            else if (other.CompareTag("Drumset"))
             {
-                Debug.Log("Play sound: " + other.transform.parent.name);
-                other.GetComponent<AudioSource>().Play();
+                other.GetComponent<AudioSource>().PlayOneShot(clip);
+                //other.GetComponent<AudioSource>().Play();
                 triggered = true;
             }
         }
