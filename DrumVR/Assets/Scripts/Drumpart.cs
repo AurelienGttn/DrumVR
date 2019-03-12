56b5510ba5530574d6561eab2614f49057acf06d
AurelienGttn <aurelien.guitton@hotmail.com>
2019-03-12 11:17:36 -0400

    Generate random sequence and check player's input

--- DrumVR/Assets/Scripts/Drumpart.cs
+++ DrumVR/Assets/Scripts/Drumpart.cs
@@ -2,26 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drumset : MonoBehaviour
+public class Drumpart : MonoBehaviour
 {
 
     private AudioSource source;
     private AudioClip sound;
+
+    // Checks if the player hits the part in the right direction
     [SerializeField] private DirectionCheck directionCheck;
 
-    // Start is called before the first frame update
+    // FX to show which part to hit
+    public ParticleSystem targetIndicator;
+
     void Start()
     {
         source = GetComponent<AudioSource>();
         sound = GetComponent<AudioSource>().clip;
+
+        // Don't show the FX at first
+        targetIndicator.Stop();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (directionCheck == null || !directionCheck.GetComponent<DirectionCheck>().wrongDirection)
         {
+            // Use PlayOneShot to be able to allow the sound to overlap
             source.PlayOneShot(sound);
-            Debug.Log(this.transform.parent.name);
         }
     }
 }
---
+++ DrumVR/Assets/Scripts/SequenceManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SequenceManager : MonoBehaviour
+{
+
+    [SerializeField] private Drumpart[] drumParts;
+
+    private List<Drumpart> randomSequence = new List<Drumpart>();
+    private List<Drumpart> playedSequence = new List<Drumpart>();
+
+    public Drumpart nextPartToHit;
+    private int currentIndex = 0;
+
+    private int mistakes = 0;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)){
+            CreateRandomSequence(1);
+        }
+    }
+
+    private void CreateRandomSequence(int size)
+    {
+        randomSequence.Clear();
+        for (int i = 0; i < size; i++)
+        {
+            int randomPart = Random.Range(0, drumParts.Length);
+            randomSequence.Add(drumParts[randomPart]);
+        }
+        nextPartToHit = randomSequence[currentIndex];
+        drumParts[ArrayUtility.IndexOf(drumParts, nextPartToHit)].targetIndicator.Play();
+    }
+
+    public void CheckPartHit(Drumpart partHit)
+    {
+        if (partHit == nextPartToHit)
+        {
+            playedSequence.Add(partHit);
+            drumParts[ArrayUtility.IndexOf(drumParts, nextPartToHit)].targetIndicator.Stop();
+            nextPartToHit = randomSequence[currentIndex++];
+            drumParts[ArrayUtility.IndexOf(drumParts, nextPartToHit)].targetIndicator.Play();
+        }
+        else
+        {
+            mistakes++;
+        }
+    }
+}
